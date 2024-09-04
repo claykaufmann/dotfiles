dd1eacb8abf8dd080e8e6a0de32077181165ed25
Clay Kaufmann <claykaufmann@gmail.com>
2024-09-04 09:03:31 -0400

    better autocomplete settings

--- nvim/.config/nvim/lua/plugins/autocomplete.lua
+++ nvim/.config/nvim/lua/plugins/autocomplete.lua
@@ -2,6 +2,7 @@ return {
   "hrsh7th/nvim-cmp",
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
+    -- fancy setup for using tab and shift + tab for selecting auto complete options
     local has_words_before = function()
       unpack = unpack or table.unpack
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -9,8 +10,17 @@ return {
     end
 
     local cmp = require("cmp")
-
+    local auto_select = false
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<CR>"] = LazyVim.cmp.confirm({ select = auto_select }),
+      ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ["<C-CR>"] = function(fallback)
+        cmp.abort()
+        fallback()
+      end,
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
