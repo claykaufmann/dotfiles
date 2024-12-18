6980edabd90b824c6a8de3a78f80d195eddfc578
Clay Kaufmann <claykaufmann@gmail.com>
2024-12-18 10:05:26 -0500

    some cool sql formatting things

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -63,7 +63,7 @@ return {
     config = function()
       -- your optional config goes here, see below.LazyVim
       require("bluloco").setup({
-        style = "light", -- "auto" | "dark" | "light"
+        style = "dark", -- "auto" | "dark" | "light"
         transparent = false,
         italics = false,
         terminal = vim.fn.has("gui_running") == 1, -- bluoco colors are enabled in gui terminals per default.
