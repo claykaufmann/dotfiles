af46a83778a340cd5ddec70450610957ca83c4c3
Clay Kaufmann <claykaufmann@gmail.com>
2025-01-24 11:48:00 -0500

    new defaults

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -9,6 +9,7 @@ return {
       flavour = "auto",
     },
   },
+
   {
     "folke/tokyonight.nvim",
     lazy = true,
@@ -22,25 +23,6 @@ return {
     },
   },
 
-  -- {
-  --   "uloco/bluloco.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   dependencies = { "rktjmp/lush.nvim" },
-  --   config = function()
-  --     -- your optional config goes here, see below.LazyVim
-  --     require("bluloco").setup({
-  --       style = "dark", -- "auto" | "dark" | "light"
-  --       transparent = false,
-  --       italics = false,
-  --       terminal = vim.fn.has("gui_running") == 1, -- bluoco colors are enabled in gui terminals per default.
-  --       guicursor = true,
-  --     })
-  --
-  --     vim.opt.termguicolors = true
-  --     vim.cmd("colorscheme bluloco")
-  --   end,
-  -- },
   {
     "navarasu/onedark.nvim",
     lazy = true,
