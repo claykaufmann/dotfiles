e5b64d41ace5af4149f4b63d577e2a799cb093be
Clay Kaufmann <claykaufmann@gmail.com>
2026-01-08 08:23:16 -0500

    reset to initial lazyvim

--- nvim/.config/nvim/lua/plugins/spec.lua
+++ nvim/.config/nvim/lua/plugins/spec.lua
@@ -1,30 +1,7 @@
 return {
-  -- the colorscheme should be available when starting Neovim
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
-
   -- I have a separate config.mappings file where I require which-key.
   -- With lazy the plugin will be automatically loaded when it is required somewhere
   { "folke/which-key.nvim", lazy = true },
-
-  {
-    "nvim-neorg/neorg",
-    -- lazy-load on filetype
-    ft = "norg",
-    -- options for neorg. This will automatically call `require("neorg").setup(opts)`
-    opts = {
-      load = {
-        ["core.defaults"] = {},
-      },
-    },
-  },
 
   {
     "dstein64/vim-startuptime",
@@ -73,15 +50,4 @@ return {
     -- mode is `n` by default. For more advanced options, check the section on key mappings
     keys = { "<C-a>", { "<C-x>", mode = "n" } },
   },
-
-  -- local plugins need to be explicitly configured with dir
-  { dir = "~/projects/secret.nvim" },
-
-  -- you can use a custom url to fetch a plugin
-  { url = "git@github.com:folke/noice.nvim.git" },
-
-  -- local plugins can also be configured with the dev option.
-  -- This will use {config.dev.path}/noice.nvim/ instead of fetching it from GitHub
-  -- With the dev option, you can easily switch between the local and installed version of a plugin
-  { "folke/noice.nvim", dev = true },
 }
