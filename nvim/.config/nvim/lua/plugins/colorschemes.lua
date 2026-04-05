cdce807247a2083c562f3d2df08abdcd5253b0d3
Clay Kaufmann <claykaufmann@gmail.com>
2026-04-05 09:57:12 -0400

    fixes for nvim 0.12

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -8,6 +8,10 @@ return {
 			transparent_background = true,
 			flavour = "frappe",
 		},
+		config = function(_, opts)
+			require("catppuccin").setup(opts)
+			vim.cmd.colorscheme("catppuccin")
+		end,
 	},
 
 	{
